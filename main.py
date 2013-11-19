c86bb763d6035fc00cc905ade829352bb36871ba
Peter Bakhirev <petro.b@gmail.com>
2013-11-18 22:43:50 -0500

    Hacking smthng together

---
+++ main.py
@@ -0,0 +1,36 @@
+import cv2
+import cv
+import subprocess
+import time
+import os
+
+class CameraImage:
+  def __init__(self, file_name):
+    self.img_gray = cv2.imread(file_name, cv.CV_LOAD_IMAGE_GRAYSCALE)
+    self.file_name = file_name
+
+  def averageLightValue(self):
+    return cv2.mean(self.img_gray)
+
+  def blurrinessValue(self):
+    img_lap = cv2.Laplacian(self.img_gray, cv.CV_16S, ksize=1, scale=1, delta=0)
+    return cv2.reduce(cv2.reduce(img_lap, 0, cv.CV_REDUCE_MAX), 1, cv.CV_REDUCE_MAX)[0][0]
+    
+
+class Camera:
+  def captureImage(self):
+    temp_file_name = "tmp/image_%d.jpg" % int(time.time())
+
+    if not subprocess.call(["/usr/bin/raspistill", "-t", "0", "-o", temp_file_name]):
+      raise Exception("Could not call raspistill!")
+
+    if not os.path.exists(temp_file_name):
+      raise Exception("Called raspistill but can't find output file!")
+
+    return CameraImage(temp_file_name)
+
+cam = Camera()
+img = cam.captureImage()
+print "File: %s" % img.file_name
+print "Light: %d" % img.averageLightValue()
+print "Blur: %d" % img.blurrinessValue()
